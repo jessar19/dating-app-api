524c551b5accd2ad73584943d0458b1bfbcac4c1
Jesus Sarmiento <jessar1996@outlook.com>
2020-04-26 02:15:52 -0500

    Added Ef test Model and Migration, Config Default connection

---
+++ DatingAPP.API/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPP.API.Data;
+using DatingAPP.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPP.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController (DataContext context)
+        {
+            _context = context;
+        }
+     
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var values =  await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+        
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Value value)
+        {
+            return Ok();
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Value value)
+        {
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DatingAPP.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPP.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPP.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }   
+    }
+}
